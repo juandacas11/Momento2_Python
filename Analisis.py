7b2c9ce3c5385e1946d3a7db198bcddf75d234a1
juandacas11 <juan.castanos@udea.edu.co>
2025-09-26 19:02:36 -0500

    test: Prueba de carga y limpieza

--- Analisis.py
+++ Analisis.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numpy as np
 from Funciones import *  # Importar las funciones desde el módulo de fun
 
-cargar_datos('Data/productos.csv')
+datos_1 =cargar_datos('Data/productos.csv')
+
+datos_2 =cargar_datos('Data/productos.json')
+
+print(datos_1)
+print(datos_2)
